d917781a1a4a5cae334ce05d30e539852316b16d
Greg Greenleaf <pacohojaverde@gmail.com>
2014-06-14 09:45:19 -0700

    Cleaned index.php, added image directory

--- index.php
+++ index.php
@@ -1,48 +1,22 @@
 <!DOCTYPE html>
-<html lang="en">
+<html>
 	<head>
 		<meta charset="utf-8">
-
-		<!-- Always force latest IE rendering engine (even in intranet) & Chrome Frame
-		Remove this if you use the .htaccess -->
-		<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
-
 		<title>Vessel Registration</title>
-		<meta name="description" content="">
-		<meta name="author" content="Ryan">
-
 		<meta name="viewport" content="width=device-width; initial-scale=1.0">
-		
-        <style>
-   			input[type="text"]
-			{
-    			width:200px
+		<style>
+			input[type="text"]{
+				width:200px
 			}
-			body
-			{
-				background: #606060; 
+			body{
+				background: #606060;
 				color: white;
 		}
-    	</style>		
+		</style>
 	</head>
-
 	<body>
-		<div>
-			<header>
-				<h1>Vessel Registration</h1>
-			</header>
-			<nav>
-				<p>
-					<a href="new.php">Register New Vessel</a>
-				</p>
-				<p>
-					<a href="existing.php">Register Existing Vessel</a>
-				</p>
-			</nav>
-
-			<div>
-
-			</div>
-		</div>
+		<h1>Vessel Registration</h1>
+		<a href="new.php">Register New Vessel</a>
+		<a href="existing.php">Register Existing Vessel</a>
 	</body>
 </html>
