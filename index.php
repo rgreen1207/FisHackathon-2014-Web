64aed08e7807b127d8802e09c8943650db7f3f30
Greg Greenleaf <pacohojaverde@gmail.com>
2014-06-14 10:01:18 -0700

    Added kelp image to index page.

--- index.php
+++ index.php
@@ -5,18 +5,30 @@
 		<title>Vessel Registration</title>
 		<meta name="viewport" content="width=device-width; initial-scale=1.0">
 		<style>
+			body{
+				background: #ffffff;
+				color: white;
+			}
+			#container{
+			
+			}
 			input[type="text"]{
 				width:200px
 			}
-			body{
-				background: #606060;
-				color: white;
-		}
+			#leftSide, #rightSide{
+				float: left;
+			}
 		</style>
 	</head>
 	<body>
-		<h1>Vessel Registration</h1>
-		<a href="new.php">Register New Vessel</a>
-		<a href="existing.php">Register Existing Vessel</a>
+		<div id="container">
+			<div id="leftSide">
+				<img id="kelpLogo" src="Images/kelp.png" alt="Main page kelp logo" />
+			</div>
+			<div id="rightSide">
+				<a href="new.php">Register New Vessel</a>
+				<a href="existing.php">Register Existing Vessel</a>
+			</div>
+		</div>
 	</body>
 </html>
