8991056754d5e59e36f76504a4ef8b2e4eb7656b
rgreen1207 <rgreen@csumb.edu>
2014-06-15 02:20:47 -0700

    colors almost work

--- admin.php
+++ admin.php
@@ -16,73 +16,20 @@ require_once 'dbConn.php';
 
      <script src="http://ajax.googleapis.com/ajax/libs/jquery/2.0.2/jquery.min.js"></script>
     <script src="//ajax.googleapis.com/ajax/libs/jqueryui/1.10.3/jquery-ui.min.js"></script>
-    <script type="text/javascript">
-$("document").ready(function(){
-  $('#RR').click(function(){
-  	$("#NewR").hide("slow");
-    $("#ReRe").show("slow");
-  });
-  $('#NR').click(function(){
-  	$("#ReRe").hide("slow");
-    $("#NewR").show("slow");
-  })
-});
-	
-	
-</script>
-<style id="jsbin-css">
- #ReRe, #NewR{
-  display: none;
-}
 </style>
 </head>
 
 <body width= "300px" height="300px" bgcolor="gray">
 
-	<button id="NR" align="center">View New Registration</button></br>
+	<!--<button id="NR" align="center">View New Registration</button></br>
 	<button id="RR"align="center">View Recurring Registration</button></br>
 	<!--<form method="POST" action="campers.php"><input type="submit" value="View Campers" id="view" align="center"/></form>
-	<form method="POST" action="camps.php"><input type="submit" value="View Camps" id="view" align="center"/></form>-->
-	<?php
+	<form method="POST" action="camps.php"><input type="submit" value="View Camps" id="view" align="center"/></form>--><?php
 	echo "<form method='POST' action='index.php'><input type='submit' value='LogOut' id='view' align='center'/></form>";
 	session_destroy();
 	?>
 	
     <div id="NewR" align="center">
-	<?php
-		$sql = "SELECT * FROM NewRegistration ORDER BY Request ASC";
-		$stmt = $dbConn->prepare($sql);
-		$stmt->execute();
-		$vessels = $stmt->fetchAll();
-		
-		
-
-		echo "<table align='center' border='1'><tr><td>Request Number</td><td>Owner's Name</td><td>Email</td><td>Phone</td><td>Address</td><td>Municipality</td><td>Gear</td><td>Vessel Type</td><td>Length</td><td>Intention</td><td>Approve/Deny</td></tr>";
-		foreach($vessels as $vessel) {
-			if($vessel['Approved_Denied'] == 0)
-			{
-				$color="red";
-			}
-			else if($vessel['Approved_Denied'] == 1)
-			{
-				$color="green";
-			}
-			else
-			{
-				$color="";		
-			}
-				echo "<tr bgcolor='".$color."'><td align='center'>" . $vessel['Request'] . "</td><td>" . $vessel['OwnerName'] ."</td><td>" . $vessel['Email'] . "</td><td>" . $vessel['Phone'] . "</td><td>";
-				echo $vessel['Address'] . "</td><td>" . $vessel['Municipality'] . "</td><td>" . $vessel['Gear'] . "</td><td>" . $vessel['VT'] . "</td><td>" . $vessel['Length'] . "</td><td>" . $vessel['Intention'] . "</td><td>";
-				echo "<form method='POST' action='color.php'><input type='submit' value='Approve' name='approve' id='approve' align='center'/>";
-				echo "<input type='submit' value='Deny' name='deny' id='deny' align='center'/></form>";
-				echo "</td></td></tr>";				
-		}
-		echo "</table>";
-?>
-
-  </div>
-  
-  <div id="ReRe" align="center">
 	<?php
 		$sql = "SELECT * FROM ExistingRegistration ORDER BY RequestExistingNum ASC";
 		$stmt = $dbConn->prepare($sql);
@@ -103,13 +50,15 @@ $("document").ready(function(){
 			}
 			else
 			{
-				$color="";
+				$color="";		
 			}
-				echo "<tr bgcolor='".$color."'><td align='center'>" . $vessel['RequestExistingNum'] . "</td><td>" . $vessel['OwnerName'] ."</td><td>" . $vessel['Email'] . "</td><td>" . $vessel['Phone'] . "</td><td>" . $vessel['Address'] . "</td><td>" . $vessel['Municipality'] . "</td><td>" . $vessel['Gear'] . "</td><td>" . $vessel['VT'] . "</td><td>" . $vessel['Length'] . "</td><td>" . $vessel['Intention'] . "</td><td><form method='POST' action='admin.php'><input type='submit' value='Approve' name='Back' align='center'/></form><form method='POST' action='admin.php'><input type='submit' value='Deny' name='Back' align='center'/></form></td></td></tr>";
+				echo "<tr bgcolor='".$color."'><td align='center'>" . $vessel['RequestExistingNum'] . "</td><td>" . $vessel['OwnerName'] ."</td><td>" . $vessel['Email'] . "</td><td>" . $vessel['Phone'] . "</td><td>";
+				echo $vessel['Address'] . "</td><td>" . $vessel['Municipality'] . "</td><td>" . $vessel['Gear'] . "</td><td>" . $vessel['VT'] . "</td><td>" . $vessel['Length'] . "</td><td>" . $vessel['Intention'] . "</td><td>";
+				echo "<form method='POST' action='color.php'><input type='submit' value='Approve' name='approve' id='approve' align='center'/>";
+				echo "<input type='submit' value='Deny' name='deny' id='deny' align='center'/></form>";
+				echo "</td></td></tr>";				
 		}
 		echo "</table>";
-
-	
 ?>
 
   </div>
--- color.php
+++ color.php
@@ -4,9 +4,17 @@
 	
 	if(isset($_POST['approve']))
 	{
-		$sql = "UPDATE ExistingRegistration SET Approved_Denied=:approval WHERE Approved_Denied='2'";
+		$sql = "UPDATE ExistingRegistration SET Approved_Denied=:approval WHERE Approved_Denied='3'";
 		$stmt = $dbConn->prepare($sql);
 		$stmt->execute(array (":approval"=>'1'));
+		header("Location: admin.php");
+	}	
+	
+	if(isset($_POST['deny']))
+	{
+		$sql = "UPDATE ExistingRegistration SET Approved_Denied=:denial WHERE Approved_Denied='3'";
+		$stmt = $dbConn->prepare($sql);
+		$stmt->execute(array (":denial"=>'0'));
 		header("Location: admin.php");
 	}		  
 ?>
