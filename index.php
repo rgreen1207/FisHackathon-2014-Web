01681e40812cd6d5b57ae8dd14c6b1f1e335ad08
Greg Greenleaf <pacohojaverde@gmail.com>
2014-06-14 18:19:01 -0700

    Redesign attempt.

--- index.php
+++ index.php
@@ -2,52 +2,25 @@
 <html lang="en">
 	<head>
 		<meta charset="utf-8">
-
-		<!-- Always force latest IE rendering engine (even in intranet) & Chrome Frame
-		Remove this if you use the .htaccess -->
-		<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
-
 		<title>Vessel Registration</title>
-		<meta name="description" content="">
-		<meta name="author" content="Ryan">
-
-		<meta name="viewport" content="width=device-width; initial-scale=1.0">
-		
-        <style>
-   			input[type="text"]
-			{
-    			width:200px
+		<style>
+			input[type="text"]{
+				width:200px;
 			}
-			body
-			{
-				background: #606060; 
+			body{
+				background: #606060;
 				color: white;
-		}
-    	</style>		
+			}
+		</style>
 	</head>
-
 	<body>
-		<div>
-			<div align="right">
-			<form method="link" action="admin.php">
-				<input align="right" type="submit" value="Admin Login">
-			</form>
+		<div id="container">
+			<div id="left">
+				<input type="submit" />
+				<input type="submit" />
 			</div>
-			<nav>
-				<table align="center"><caption align="center" style="font-size: 2em"><h1>Vessel Registration</h1></caption><tr>
-					<td align="center" style="font-size:2em">Re-Register a Vessel</td>
-					<td td align="center" style="font-size:2em">Register a New Vessel</td></tr><tr>
-					<td>
-						<a href="existing.php" onclick=existing.php><img src="images/rereg.jpg" width="500px" height="500px"></a> 
-					</td>
-					<td>
-						<a href="new.php" onclick=new.php><img src="images/new.jpg" width="500px" height="500px"></a>
-					</td>					
-				</tr></table>
-			</nav>
-
-			<div>
-
+			<div id="right">
+				<img alt="blah" src="images/kelp.png">
 			</div>
 		</div>
 	</body>
